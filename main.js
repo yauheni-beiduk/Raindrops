3f4b13c3f5d597264e86d9fe9da5a23b1cb7df4f
yauheni-beiduk <yauheni.beiduk@gmail.com>
2020-12-14 20:27:10 +0300

    feat: add class JS

--- main.js
+++ main.js
@@ -12,22 +12,32 @@ let answerCorrect = false;
 //Play song
 //song.play();
 
+// RANDOM NUMBER AND OPERATOR
+function getRandomEquation() {
 
-
-function getRandomNumber(min, max) {
+    function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; 
-};
-getRandomNumber(min, max);
+    };
 
-function getRandomOperator(){
-    let operators = Array('+','-','*','/');
-    let item = operators[Math.floor(Math.random()*operators.length)];
-    return item
-};
-getRandomOperator();
+    function getRandomOperator(){
+        let operators = Array('+','-','*');
+        let operator = operators[Math.floor(Math.random()*operators.length)];
+        return operator
+    };
 
+    const x = getRandomNumber(min, max);
+    const y = getRandomOperator();
+    const z = getRandomNumber(min, max);
+    if (x>z) {
+        return (x + y + z)
+    }
+        return (z + y + x)
+}
+
+
+// CLICK CALCULATOR
 
 function numbersClick(number) {
     if (result.textContent === '0') {
@@ -52,7 +62,7 @@ function deleteNumber() {
     if(result.textContent.length > 1 ) {
     return result.textContent = result.textContent.slice(0, result.textContent.length-1);
     }
-    else return  result.textContent = '0';
+    return  result.textContent = '0';
 }
 
 // function btnClick(btns) {
@@ -60,6 +70,23 @@ function deleteNumber() {
 
 // }
 // btns.forEach(number => number.addEventListener('keypress',  resultScreen ));  
+
+class Drop {
+constructor(speed, equation, res, color) {
+
+this.speed = speed;
+this.equation = equation;
+this.res = res;
+this.color = color;
+}
+createDrop() {
+    let div = document.createElement('div');
+    div.className = "raindrop";
+    document.body.append(div);
+}
+};
+
+
 
 numbersBtn.forEach(number => number.addEventListener('click',  resultScreen ));
 clearBtn.addEventListener('click', clearScreen);
