1cc63a0a4b5c8ae363d0e4b3823e5dacb4fc92d6
Yauheni Beiduk <yauheni.beiduk@gmail.com>
2020-12-16 15:49:55 +0300

    feat:add image and css classes

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const result = document.getElementById('result');
 const song = document.querySelector('.sound');
 const wave = document.getElementById('.wave');
 const drop = document.getElementById('drop');
+const scoreTable = document.getElementById('score-table');
 
+let score = 0;
 let answerCorrect = false;
 
 
@@ -77,38 +79,59 @@ function deleteNumber() {
 
 
 
-class Drop {
-    constructor(speed, equation=getRandomEquation(), res, color) {
+// class Drop {
+//     constructor(speed, equation=getRandomEquation(), res, color) {
 
-    this.speed = speed;
-    this.equation = equation;
-    this.res = res;
-    this.color = color;
-    }
-    createDrop() {
-        const drop = document.getElementById('drop');
-        let div = document.createElement('div');
-        div.className = "raindrop";
-        drop.appendChild(div);
-        div.style.left =Math.random()*90+'%';
-        div.append(this.equation);
-        const x = eval(this.equation);
-        return x;
-    }
-};
+//     this.speed = speed;
+//     this.equation = equation;
+//     this.res = res;
+//     this.color = color;
+//     }
+//     createDrop() {
+//         const drop = document.getElementById('drop');
+//         let div = document.createElement('div');
+//         div.className = "raindrop";
+//         drop.appendChild(div);
+//         div.style.left =Math.random()*90+'%';
+//         div.append(this.equation);
+//         const x = eval(this.equation);
+//         return x;
+//     }
+// };
 
 
+
+function createDrop() {
+    const equation = getRandomEquation();
+    const drop = document.getElementById('drop');
+    let div = document.createElement('div');
+    div.className = "raindrop";
+    drop.appendChild(div);
+    div.style.left =Math.random()*90+'%';
+    div.append(equation);
+    const x = eval(equation);
+    return x;
+}
 
 const arrayResult = [];
 function arrays() {
-arrayResult.push(new Drop().createDrop());
+arrayResult.push(createDrop());
 return arrayResult;
 }
 
+const raindrop = document.getElementsByClassName('raindrop');
+
+// setInterval(arrays, 4000);
 
 function enterNumber() {
-    if(arrayResult[arrayResult.length-1] == result.textContent) {
-    console.log(true);
+    if(arrayResult[0] == result.textContent) {
+    let score = 10 ;
+    score++
+    arrayResult.splice(0,1);
+    result.textContent = '0';
+    drop.firstChild.classList.add('boom');
+    drop.firstChild.remove();
+    scoreTable.textContent = score;
     }
     else console.log(false);
 }
